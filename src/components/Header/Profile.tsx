9ff194d59c69cf494f14dfc3613b6fc6892cf8e7
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-18 14:04:43 -0300

    feat: add responsive to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Ives Moreira</Text>
-        <Text color="gray.300" fontSize="small">
-          ivesmoreira3@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Ives Moreira</Text>
+          <Text color="gray.300" fontSize="small">
+            ivesmoreira3@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Ives Moreira" src="https://github.com/ives-gomes.png" />
     </Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
 
 import Profile from '@components/Header/Profile'
 import NotificationsNav from '@components/Header/NotificationsNav'
@@ -6,6 +6,11 @@ import SearchBox from '@components/Header/SearchBox'
 import Logo from '@components/Header/Logo'
 
 export default function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Flex
       as="header"
@@ -19,12 +24,12 @@ export default function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
 
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   )

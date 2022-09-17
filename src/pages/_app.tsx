85d1d53af5a6e46e4c15fbfcaa466a771bd12830
Ives Moreira <ivesmoreira3@gmail.com>
2022-09-17 14:07:54 -0300

    feat: add miragejs config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { SidebarDrawerProvider } from '@contexts/SidebarDrawerContext'
+import { makeServer } from '@services/mirage'
 
 import { theme } from '@styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
